bb35960a9f81302ee7a25b5ad04502b307ee9e59
RedheatWei <qjyyn@qq.com>
2019-09-02 23:17:51 +0800

    update default theme

--- AEFTheme_Default/config.lua
+++ AEFTheme_Default/config.lua
@@ -15,11 +15,10 @@ AzerothElfConfig["AEFTheme_Default"] = {
                 "Interface\\AddOns\\AEFTheme_Default\\sounds\\笑声.ogg",
             },
             img = {
-                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+                "Interface\\AddOns\\AEFTheme_Default\\img\\哈士奇.tga",
             },
             text = {
                 "66666666",
-                "卧槽",
                 "gg",
                 "game over "
             }
@@ -30,41 +29,39 @@ AzerothElfConfig["AEFTheme_Default"] = {
                 "Interface\\AddOns\\AEFTheme_Default\\sounds\\大圣出场.ogg",
             },
             img = {
-                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+                "Interface\\AddOns\\AEFTheme_Default\\img\\哈士奇.tga",
             },
             text = {
                 "66666666",
-                "xxxxxxxx",
-                "卧槽"
             }
         },
-        {
-            event = "PLAYER_ENTERING_WORLD",
-            sounds = {
-                "Interface\\AddOns\\AEFTheme_Default\\sounds\\小霸王开始游戏.ogg",
-                "Interface\\AddOns\\AEFTheme_Default\\sounds\\新闻联播.ogg",
-            },
-            img = {
-                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
-            },
-            text = {
-                "66666666",
-                "xxxxxxxx",
-                "卧槽"
-            }
-        },
+--        {
+--            event = "PLAYER_ENTERING_WORLD",
+--            sounds = {
+--                "Interface\\AddOns\\AEFTheme_Default\\sounds\\小霸王开始游戏.ogg",
+--                "Interface\\AddOns\\AEFTheme_Default\\sounds\\新闻联播.ogg",
+--            },
+--            img = {
+--                "Interface\\AddOns\\AEFTheme_Default\\img\\哈士奇.tga",
+--            },
+--            text = {
+--                "66666666",
+--                "xxxxxxxx",
+--                "卧槽"
+--            }
+--        },
         {
             event = "BOSS_KILL",
             sounds = {
                 "Interface\\AddOns\\AEFTheme_Default\\sounds\\卡丁车胜利.ogg",
             },
             img = {
-                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+                "Interface\\AddOns\\AEFTheme_Default\\img\\哈士奇.tga",
             },
             text = {
                 "66666666",
-                "xxxxxxxx",
-                "卧槽"
+                "卧槽",
+                "黑手别摸"
             }
         },
         {
@@ -73,12 +70,10 @@ AzerothElfConfig["AEFTheme_Default"] = {
                 "Interface\\AddOns\\AEFTheme_Default\\sounds\\愤怒的小鸟通关.ogg",
             },
             img = {
-                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+                "Interface\\AddOns\\AEFTheme_Default\\img\\哈士奇.tga",
             },
             text = {
                 "66666666",
-                "xxxxxxxx",
-                "卧槽"
             }
         },
         {
@@ -87,12 +82,10 @@ AzerothElfConfig["AEFTheme_Default"] = {
                 "Interface\\AddOns\\AEFTheme_Default\\sounds\\超级玛丽开始.ogg",
             },
             img = {
-                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+                "Interface\\AddOns\\AEFTheme_Default\\img\\哈士奇.tga",
             },
             text = {
-                "66666666",
-                "xxxxxxxx",
-                "卧槽"
+                "有人侵入了炎魔之王的领地，把他们揪出来，让他们受罚",
             }
         },
         {
@@ -101,12 +94,10 @@ AzerothElfConfig["AEFTheme_Default"] = {
                 "Interface\\AddOns\\AEFTheme_Default\\sounds\\妲己.ogg",
             },
             img = {
-                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+                "Interface\\AddOns\\AEFTheme_Default\\img\\哈士奇.tga",
             },
             text = {
-                "66666666",
-                "xxxxxxxx",
-                "卧槽"
+                "我的钱多的能买下整个藏宝海湾",
             }
         },
         {
@@ -116,12 +107,10 @@ AzerothElfConfig["AEFTheme_Default"] = {
                 "Interface\\AddOns\\AEFTheme_Default\\sounds\\坦克大战.ogg",
             },
             img = {
-                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+                "Interface\\AddOns\\AEFTheme_Default\\img\\哈士奇.tga",
             },
             text = {
-                "66666666",
-                "xxxxxxxx",
-                "卧槽"
+                "新的一波送死开始了",
             }
         },
         {
@@ -130,12 +119,29 @@ AzerothElfConfig["AEFTheme_Default"] = {
                 "Interface\\AddOns\\AEFTheme_Default\\sounds\\马里奥跳跃.ogg",
             },
             img = {
-                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+                "Interface\\AddOns\\AEFTheme_Default\\img\\哈士奇.tga",
+
             },
             text = {
-                "66666666",
-                "xxxxxxxx",
-                "卧槽"
+                "快跑吧小姑娘，快跑",
+            }
+        },
+        {
+            event = "NEW_MOUNT_ADDED",
+            sounds = {
+                "Interface\\AddOns\\AEFTheme_Default\\sounds\\筋斗云.ogg",
+            },
+            text = {
+                "快开车",
+            }
+        },
+        {
+            event = "QUEST_COMPLETE",
+            sounds = {
+                "Interface\\AddOns\\AEFTheme_Default\\sounds\\回答正确.ogg",
+            },
+            text = {
+                "完成了这一个，还会有另一个",
             }
         },
     }
