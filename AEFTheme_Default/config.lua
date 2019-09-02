fb8162d1ddb077c967aec9ad5e94bd3fb761a68f
RedheatWei <qjyyn@qq.com>
2019-09-02 18:15:25 +0800

    add new element to default theme

--- AEFTheme_Default/config.lua
+++ AEFTheme_Default/config.lua
@@ -12,6 +12,7 @@ AzerothElfConfig["AEFTheme_Default"] = {
             sounds = {
                 "Interface\\AddOns\\AEFTheme_Default\\sounds\\超级玛丽死亡.ogg",
                 "Interface\\AddOns\\AEFTheme_Default\\sounds\\周星驰大笑.ogg",
+                "Interface\\AddOns\\AEFTheme_Default\\sounds\\笑声.ogg",
             },
             img = {
                 "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
@@ -27,6 +28,106 @@ AzerothElfConfig["AEFTheme_Default"] = {
             event = "PLAYER_LEVEL_UP",
             sounds = {
                 "Interface\\AddOns\\AEFTheme_Default\\sounds\\大圣出场.ogg",
+            },
+            img = {
+                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+            },
+            text = {
+                "66666666",
+                "xxxxxxxx",
+                "卧槽"
+            }
+        },
+        [2] = {
+            event = "PLAYER_ENTERING_WORLD",
+            sounds = {
+                "Interface\\AddOns\\AEFTheme_Default\\sounds\\小霸王开始游戏.ogg",
+                "Interface\\AddOns\\AEFTheme_Default\\sounds\\新闻联播.ogg",
+            },
+            img = {
+                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+            },
+            text = {
+                "66666666",
+                "xxxxxxxx",
+                "卧槽"
+            }
+        },
+        [3] = {
+            event = "BOSS_KILL",
+            sounds = {
+                "Interface\\AddOns\\AEFTheme_Default\\sounds\\卡丁车胜利.ogg",
+            },
+            img = {
+                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+            },
+            text = {
+                "66666666",
+                "xxxxxxxx",
+                "卧槽"
+            }
+        },
+        [4] = {
+            event = "ACHIEVEMENT_EARNED",
+            sounds = {
+                "Interface\\AddOns\\AEFTheme_Default\\sounds\\愤怒的小鸟通关.ogg",
+            },
+            img = {
+                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+            },
+            text = {
+                "66666666",
+                "xxxxxxxx",
+                "卧槽"
+            }
+        },
+        [5] = {
+            event = "ZONE_CHANGED_NEW_AREA",
+            sounds = {
+                "Interface\\AddOns\\AEFTheme_Default\\sounds\\超级玛丽开始.ogg",
+            },
+            img = {
+                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+            },
+            text = {
+                "66666666",
+                "xxxxxxxx",
+                "卧槽"
+            }
+        },
+        [6] = {
+            event = "AUCTION_HOUSE_SHOW",
+            sounds = {
+                "Interface\\AddOns\\AEFTheme_Default\\sounds\\妲己.ogg",
+            },
+            img = {
+                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+            },
+            text = {
+                "66666666",
+                "xxxxxxxx",
+                "卧槽"
+            }
+        },
+        [7] = {
+            event = "PLAYER_ENTERING_BATTLEGROUND",
+            sounds = {
+                "Interface\\AddOns\\AEFTheme_Default\\sounds\\世界杯赛前.ogg",
+                "Interface\\AddOns\\AEFTheme_Default\\sounds\\坦克大战.ogg",
+            },
+            img = {
+                "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
+            },
+            text = {
+                "66666666",
+                "xxxxxxxx",
+                "卧槽"
+            }
+        },
+        [7] = {
+            event = "PLAYER_ENTER_COMBAT",
+            sounds = {
+                "Interface\\AddOns\\AEFTheme_Default\\sounds\\马里奥跳跃.ogg",
             },
             img = {
                 "Interface\\AddOns\\AEFTheme_Default\\img\\1.tga",
